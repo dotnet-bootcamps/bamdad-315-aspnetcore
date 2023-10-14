89c9b6a5bec7c653b7418ab17c7366e58e0e055c
msavarian <sav68.net@gmail.com>
2023-10-15 00:23:44 +0330

    Add project files.

---
+++ S13/S13-ContactUsForm/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace S13_ContactUsForm.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ S13/S13-ContactUsForm/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace S13_ContactUsForm.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
